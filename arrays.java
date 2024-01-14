81834e94396a69e7fe35f3220e5e1c619de2c48e
yogesh5454 <84499534+yogesh5454@users.noreply.github.com>
2024-01-14 10:05:59 -0800

    commit

---
+++ arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0]=55;
+        marks[1]=45;
+        marks[2]=90;
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
